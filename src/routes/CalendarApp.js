171a68766248c7b1d7c57e9ab79fb0dfd37fe8af
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-31 04:44:55 +0100

    bookinf component

---
+++ src/routes/CalendarApp.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react"
+import Calendar from "react-calendar"
+import "react-calendar/dist/Calendar.css"
+
+function CalendarApp() {
+  const [date, setDate] = useState(new Date())
+  const [userDate, setUserDate] = useState("")
+  console.log(date.getMonth(), date.getDay())
+
+  useEffect(() => {
+    setUserDate(`${date.getDate()}.${date.getMonth() + 1}`)
+
+  }, [date])
+
+  return (
+    <div className="calendar-app">
+      <div>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+
+      <section>
+        <h2>Dostępne daty</h2>
+        <p>Wybrana data: {userDate}</p>
+      </section>
+    </div>
+  )
+}
+
+export default CalendarApp
