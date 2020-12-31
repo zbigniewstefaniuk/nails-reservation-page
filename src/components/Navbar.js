e665398665801dd6fd1dbe6ef3fe6434ea2f181f
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-31 15:25:31 +0100

    added simple navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import logo from '../images/logo.png'
+
+function Navbar() {
+    return (
+        <div className='top-navbar'>
+            <img className="logo" src={logo} alt="logo-Joanna" />
+            <div className='navbar-links'>
+                <a href='#'>Cennik</a>
+                <a href='#'>Umów się</a>
+                <a href='#'>Kontakt</a>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
