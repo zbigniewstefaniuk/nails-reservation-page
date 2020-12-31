c66e8b225d1b151a5bc0e515d3a4679f2bbbc84a
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-31 04:44:25 +0100

    booking system component

---
+++ src/components/GetPhone.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import CalendarApp from "../routes/CalendarApp"
+
+function GetPhone() {
+  const clickHandler = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <Router>
+      <div className="call-me">
+        <div>
+          <h1>JUŻ TERAZ UMÓW SIĘ NA WIZYTĘ</h1>
+          <button className="order-nails" onClick={clickHandler}>
+            <Link to="/calendar"> Umów się</Link>{" "}
+          </button>
+          <Switch>
+            <Route path="/calendar" component={CalendarApp}></Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default GetPhone
