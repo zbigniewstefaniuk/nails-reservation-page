e9303e94575803feb640a56c12ca048fe493dd18
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-02 01:01:05 +0100

    exported nav items to separeted file

---
+++ src/components/Navbar/NavbarItems.js
@@ -0,0 +1,22 @@
+export const NavbarItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Umów się',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Cennik',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Kontakt',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
