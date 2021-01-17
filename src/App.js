47e786b6f6bc05476d20f8a70d289970f2f3f4b5
ZaynRemote <zbyszek791@hotmail.com>
2021-01-17 20:34:28 +0100

    added id's to components for scroolling to them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css"
 
 import logo from "./images/logo.png"
 
+//
+import { animateScroll as scroll } from "react-scroll"
+
 // components
 import Images from "./components/Images"
 import BookNails from "./components/BookNails"
@@ -12,21 +15,21 @@ import Navbar from "./components/Navbar/Navbar"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" id="home">
         <Navbar />
         <div className="header">
-          <img className="logo" src={logo} alt="logo-Joanna" />
+          <img className="logo" src={logo} alt="logo-Joanna"  onClick={() => scroll.scrollToTop}/>
         </div>
         <br />
         <Images />
       </header>
-      <header className="App-header">
+      <header className="App-header" id="book-nails">
         <BookNails />
       </header>
-      <header className="App-header">
+      <header className="App-header" id="prices">
         <Prices />
       </header>
-      <header className="App-header">
+      <header className="App-header" id="contact">
         <Contact />
       </header>
     </div>
