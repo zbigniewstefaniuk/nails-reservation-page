47514c46a7fe2a9346c47ad3ce80bbc14c94bd91
ZaynRemote <zbyszek791@hotmail.com>
2021-01-17 21:33:40 +0100

    separeted data from prices component

---
+++ src/components/Prices/PricesData.js
@@ -0,0 +1,32 @@
+export const Pricesdata = [
+    {
+        price: '50 zł',
+        title: 'Manicure Hybrydowy',
+        cName: 'nav-links'
+    },
+    {
+        price: '70 zł',
+        title: 'Manicure żelowy',
+        cName: 'nav-links'
+    },
+    {
+        price: '80 zł',
+        title: 'Przedłużenie paznokci metodą żelową',
+        cName: 'nav-links'
+    },
+    {
+        price: '60 zł',
+        title: 'Manicure hybrydowy z bazą proteinową',
+        cName: 'nav-links'
+    },
+    {
+        price: '60 zł',
+        title: 'Uzupełnianie paznokci hybrydowych',
+        cName: 'nav-links'
+    },
+    {
+        price: '50 zł',
+        title: 'Uzupełnianie paznokci żelowych',
+        cName: 'nav-links'
+    }
+]
