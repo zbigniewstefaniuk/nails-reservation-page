1f2b7e72df18c9bbf46dffde8997baf5d5c6e12b
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-30 16:51:45 +0100

    added phone contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ function Footer() {
     <div>
       <h1>Kontakt</h1>
       <h2>joannafajto95@gmail.com</h2>
+      <h2>+48 512 602 455</h2>
       <div className='insta-photos'>
           <h1>Instagram</h1>
         {/* <Feed
