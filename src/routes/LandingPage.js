6e3c4f74f69dc5461d454ba6f7cbba2189fdb3ee
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 23:22:52 +0100

    init commit

---
+++ src/routes/LandingPage.js
@@ -0,0 +1,15 @@
+import Contact from "../components/Contact"
+import Prices from "../components/Prices/Prices"
+import Home from "../components/Home"
+
+function LandingPage() {
+    return (
+        <>
+            <Home />
+            <Prices />
+            <Contact />
+        </>
+    )
+}
+
+export default LandingPage
