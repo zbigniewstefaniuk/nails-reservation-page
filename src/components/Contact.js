58571abc546c7f22d5e8a631fef60ee7db6423eb
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 23:22:19 +0100

    added id to section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,20 +4,20 @@ import styles from '../styles/contact.module.css'
 
 function Footer() {
   return (
-    <section className={styles.contactMainSection}>
+    <section className={styles.contactMainSection} id="contact">
       <div className={styles.contactInfo}>
-      <h1>Kontakt</h1>
-      <h2>joannafajto95@gmail.com</h2>
-      <h2>+48 512 602 455</h2>
-      <div className='insta-photos'>
+        <h1>Kontakt</h1>
+        <h2>joannafajto95@gmail.com</h2>
+        <h2>+48 512 602 455</h2>
+        <div className='insta-photos'>
           <h1>Instagram</h1>
-        {/* <Feed
+          {/* <Feed
           userName="fajtojoanna"
           className="Feed"
           classNameLoading="Loading"
           limit="3"
         /> */}
-      </div>
+        </div>
       </div>
     </section>
   )
