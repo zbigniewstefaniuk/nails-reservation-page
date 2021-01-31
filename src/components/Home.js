d8631e47e772d153233452bfe787c550502656cc
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 16:27:03 +0100

    init commit of landing page hero section

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import styles from '../styles/home.module.css'
+import Navbar from "./Navbar/Navbar"
+
+function Home() {
+    return (
+        <section className={styles.homeSection}>
+            <Navbar />
+            <div className={styles.homeSectionLanding}>
+                <div className={styles.homeHeroText}>
+                    <h1>Umów się na paznokcie juz teraz!</h1>
+                </div>
+                <div className={styles.bookNails}>
+                    <button className={styles.homeButton}>
+                        Umów się!
+            </button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Home
