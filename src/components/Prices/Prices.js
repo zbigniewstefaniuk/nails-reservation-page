b127496a5def8b7f492d71e1c4b9f470fcde899f
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 21:09:39 +0100

    changed from bootstrap to my css

--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -1,22 +1,23 @@
 import { Pricesdata } from "./PricesData"
+import styles from '../../styles/prices.module.css'
 
 function Prices() {
   return (
-    <>
+    <section className={styles.prices}>
       <div class="container my-5">
         <div class="text-center mb-5">
-          <h1 class="text-capitalize font-weight-bold">Cennik</h1>
+          <h1>Cennik</h1>
         </div>
 
         {Pricesdata.map(({ price, title, cName }, index) => {
           return (
             <div
               key={index}
-              class="my-5 py-4 px-5 bg-light d-sm-flex align-items-center justify-content-between"
+              className={styles.pricesContainerService}
               style={{ borderRadius: "30px" }}>
               <div class="text-center">
                 <span style={{ fontWeight: 600 }}></span>
-                <h1 class="my-2 font-weight-bold">{price}</h1>
+                <h2 class="my-2 font-weight-bold">{price}</h2>
               </div>
               <div class="text-center text-uppercase text-secondary">
                 <medium class="d-block" style={{ fontWeight: 600 }}>
@@ -26,7 +27,7 @@ function Prices() {
               <div class="text-center">
                 <a
                   onClick={() => alert("Funkcja dostepna wkrótce...")}
-                  href="#"
+                  href=""
                   class="btn text-white px-5 py-3"
                   style={{
                     backgroundColor: "#282c34",
@@ -39,7 +40,7 @@ function Prices() {
           )
         })}
       </div>
-    </>
+    </section>
   )
 }
 
