52af71b51c4553c5bd2e70ac2e487dc7555d4233
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 22:06:53 +0100

    restructurized priceses list

--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -13,28 +13,15 @@ function Prices() {
           return (
             <div
               key={index}
-              className={styles.pricesContainerService}
-              style={{ borderRadius: "30px" }}>
-              <div class="text-center">
-                <span style={{ fontWeight: 600 }}></span>
-                <h2 class="my-2 font-weight-bold">{price}</h2>
-              </div>
+              className={styles.pricesContainerService}>
               <div class="text-center text-uppercase text-secondary">
                 <medium class="d-block" style={{ fontWeight: 600 }}>
                   {title}
                 </medium>
               </div>
               <div class="text-center">
-                <a
-                  onClick={() => alert("Funkcja dostepna wkrótce...")}
-                  href=""
-                  class="btn text-white px-5 py-3"
-                  style={{
-                    backgroundColor: "#282c34",
-                    borderRadius: "30px",
-                  }}>
-                  Umów się
-                </a>
+                <span style={{ fontWeight: 600 }}></span>
+                <h2 class="my-2 font-weight-bold">{price}</h2>
               </div>
             </div>
           )
