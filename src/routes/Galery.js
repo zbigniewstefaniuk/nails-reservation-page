7fa177a62a2edfbe34def46b2472d44927e21824
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 23:24:57 +0100

    added images component to render in galery

--- src/routes/Galery.js
+++ src/routes/Galery.js
@@ -1,9 +1,11 @@
+import Images from "../components/Images";
 
 function Galery() {
 
     return (
         <div>
             <h1>Tu bedzie Galeria :)</h1>
+            <Images />
         </div>
     )
 }
