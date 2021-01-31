cb7edb107f55d935dafc9f6e28d44239b272db88
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-01-31 23:21:40 +0100

    added if statment for links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import './Navbar.css'
-import { NavbarItems } from './NavbarItems'
-
 import { Link } from "react-scroll"
+import { Link as LinkRoute } from "react-router-dom"
+
+import './Navbar.css'
+
+import { NavbarItems } from './NavbarItems'
+import Galery from '../../routes/Galery'
 
 function Navbar() {
     const [menu, setMenu] = useState({ clicked: false })
@@ -18,13 +21,25 @@ function Navbar() {
             </div>
             <ul className={menu.clicked ? 'nav-menu active' : 'nav-menu'}>
                 {NavbarItems.map(({ title, url, cName }, index) => {
-                    return (
-                        <li key={index}>
-                            <Link className={cName} to={url} smooth={true} duration={1000}>
-                                {title}
-                            </Link>
-                        </li>
-                    )
+                    if (url === '/galery') {
+                        return (
+                            <li key={index}>
+                                <LinkRoute to={url} >
+                                    {title}
+                                </LinkRoute>
+                            </li>
+                        )
+                    } else {
+                        return (
+                            <li key={index}>
+                                <LinkRoute to={url} >
+                                    <Link className={cName} to={url} smooth={true} duration={1000}>
+                                        {title}
+                                    </Link>
+                                </LinkRoute>
+                            </li>
+                        )
+                    }
                 })}
             </ul>
         </nav>
