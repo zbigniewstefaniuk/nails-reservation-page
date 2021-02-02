ec17add3dbbd9bc9d6a687ee1db84accdf3ecf40
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-02-03 00:14:15 +0100

    navbar moved to layout component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom'
 
 import styles from '../styles/home.module.css'
-import Navbar from './Navbar/Navbar'
 
 
 function Home() {
     return (
         <section className={styles.homeSection}>
-            <Navbar />
             <div className={styles.homeSectionLanding}>
                 <div className={styles.homeHeroText}>
                     <h1>Umów się na paznokcie juz teraz!</h1>
