754cb657697c67f4a534819c4b88d53b3a1b4253
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-02-02 23:26:41 +0100

    gallery is by 2 LL!

---
+++ src/components/GalleryComp.js
@@ -0,0 +1,14 @@
+import Images from "./Images"
+
+import styles from '../styles/Gallery.css'
+
+function GalleryComp() {
+    return (
+        <div>
+                        <h1>Tu bedzie Galeria 😂</h1>
+            <Images />
+        </div>
+    )
+}
+
+export default GalleryComp
