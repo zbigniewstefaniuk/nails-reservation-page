ad1b281e1cbe0a615a647b6d0b50255dd3910f3a
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-02-03 00:14:29 +0100

    added layout component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import CalendarApp from "./routes/CalendarApp"
 import Error404Handler from "./routes/Error404Handler"
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Layout from "./components/Layout/Layout"
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path='/booknail' component={CalendarApp} />
-          <Route component={Error404Handler} />
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path='/booknail' component={CalendarApp} />
+            <Route component={Error404Handler} />
+          </Switch>
+        </Layout>
       </div>
     </Router>
   )
