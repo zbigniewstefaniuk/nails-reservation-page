77a8e3cdab4bb62a51289ab4a24da2a9fea664b6
zbigniewstefaniuk <zbyszek791@hotmail.com>
2021-02-03 00:13:34 +0100

    created Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+
+function Layout({ children }) {
+    return (
+        <>
+            <Navbar />
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
+
+export default Layout
