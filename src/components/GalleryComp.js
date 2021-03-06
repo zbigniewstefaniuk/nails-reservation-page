cfafb293a9cc7e60ea70be89c1ea0350d2e831d8
ZaynRemote <zbyszek791@hotmail.com>
2021-03-06 18:20:29 +0100

    add: header text

--- src/components/GalleryComp.js
+++ src/components/GalleryComp.js
@@ -1,16 +1,16 @@
 import Images from "./Images"
 
-import styles from '../styles/Gallery.module.css'
+import styles from "../styles/Gallery.module.css"
 
 function GalleryComp() {
-    return (
-        <div>
-            <h1>Tu bedzie Galeria 😂</h1>
-            <div className={styles.main_images_container}>
-            <Images />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className={styles.header}>Galeria</h1>
+      <div className={styles.main_images_container}>
+        <Images />
+      </div>
+    </div>
+  )
 }
 
 export default GalleryComp
