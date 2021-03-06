eb2320061c14d7429096ef43c409c72718903aa9
Zbigniew Stefaniuk <zbyszek791@hotmail.com>
2021-03-06 02:51:32 +0100

    add: photo on bg for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,23 @@ import Feed from "react-instagram-authless-feed"
 
 import styles from '../styles/contact.module.css'
 
+import contactBgImg from '../assets/contactBgImg.png'
+
 function Footer() {
   return (
     <section className={styles.contactMainSection} id="contact">
+    
       <div className={styles.header}>
       <h1>Kontakt</h1>
       </div>
+        <img className={styles.photo} src={contactBgImg} />
       <div className={styles.contactInfo}>
-        <h2>joannafajto95@gmail.com</h2>
-        <h2>+48 512 602 455</h2>
+        <p3>joannafajto95@gmail.com</p3>
+        <p3>+48 512 602 455</p3>
         <div className='insta-photos'>
         <a href="https://www.instagram.com/fajtojoanna/" >
 
-          <h2>Instagram</h2>
+          <p3>Instagram</p3>
         </a>
           {/* <Feed
           userName="fajtojoanna"
